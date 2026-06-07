aa1db91ad4f87d82ef5b11d1fc31e1392d4e17bf
infinity-coder-ai <dheerajwills@gmail.com>
2026-06-07 17:38:14 +0530

    add gitignore

--- app/config.py
+++ app/config.py
@@ -3,23 +3,23 @@ from pydantic_settings import BaseSettings
 from pydantic import Field, ConfigDict
 
 class Settings(BaseSettings):
-    database_hostname: str          
-    database_port: str
-    database_username: str
-    database_password: str
-    database_name: str
-    secret_key: str
-    algorithm: str
-    access_token_expire_minutes: int
-    cloudinary_cloud_name :str
-    cloudinary_api_key : str
-    cloudinary_api_secret :str
-    smtp_server: str = Field(..., alias="SMTP_SERVER")
-    smtp_port: int = Field(..., alias="SMTP_PORT")
-    smtp_email: str = Field(..., alias="SMTP_EMAIL")
-    smtp_password: str = Field(..., alias="SMTP_PASSWORD")
-    from_name: str = Field("HomeEase", alias="FROM_NAME")
-    
+    database_hostname: str = Field(..., env="DATABASE_HOSTNAME")
+    database_port: int = Field(..., env="DATABASE_PORT")
+    database_username: str = Field(..., env="DATABASE_USERNAME")
+    database_password: str = Field(..., env="DATABASE_PASSWORD")
+    database_name: str = Field(..., env="DATABASE_NAME")
+    secret_key: str = Field(..., env="SECRET_KEY")
+    algorithm: str = Field(..., env="ALGORITHM")
+    access_token_expire_minutes: int = Field(..., env="ACCESS_TOKEN_EXPIRE_MINUTES")
+    cloudinary_cloud_name: str = Field(..., env="CLOUDINARY_CLOUD_NAME")
+    cloudinary_api_key: str = Field(..., env="CLOUDINARY_API_KEY")
+    cloudinary_api_secret: str = Field(..., env="CLOUDINARY_API_SECRET")
+    smtp_server: str = Field(..., env="SMTP_SERVER")
+    smtp_port: int = Field(..., env="SMTP_PORT")
+    smtp_email: str = Field(..., env="SMTP_EMAIL")
+    smtp_password: str = Field(..., env="SMTP_PASSWORD")
+    from_name: str = Field("HomeEase", env="FROM_NAME")
+
 
 
 
@@ -27,7 +27,10 @@ class Settings(BaseSettings):
     model_config = ConfigDict(
         env_file=".env",
         extra="ignore",
+        case_sensitive=False,
+        populate_by_name=True,
     )
 
 
-settings = Settings()  
+settings = Settings()
+
